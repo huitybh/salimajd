ed8e8366f559fa41b3bf47a837d1d201872f9550
huitybh <133495625+huitybh@users.noreply.github.com>
2023-05-13 11:21:59 +0100

    Add files via upload

---
+++ Espace Patients.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espace Patients</title>
+    <link  rel="stylesheet"  href="font esome/css/all.min.css">
+    <link  rel="stylesheet"  href="Espace Patients.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+               
+               <a href="pprojet.php">
+                  <i class="free fa-solid fa-house" ></i>
+                  <span class="menu">Home</span>
+               </a>
+              </li>
+            <li>
+
+             <a href="#">
+                <i class="free fa-regular fa-clock"></i>
+                <span class="menu">PRENDRE RENDEZ-VOUS</span>
+             </a>
+            </li>
+            <li>
+             <a href="#">
+               <i class="free fa-regular fa-id-card"></i>
+                <span class="menu">MON PROFIL</span>
+             </a>
+            </li>
+            <li>
+             <a href="#">
+                <i class=" free fa-solid fa-calendar-days"></i>
+                <span class="menu">MES RENDEZ-VOUS</span>
+             </a>
+            </li>
+            <li>
+           
+             <a href="#">
+               <i class="free fa-solid fa-circle-info"></i>
+               <span class="menu">AIDE</span>
+             </a>
+            </li>
+            <li>
+             <a href="#">
+                <i class="free fa-solid fa-shuffle"></i>
+                <span class="menu">DECONNEXION</span>
+             </a>
+            </li>
+
+
+        </ul>
+    </nav>
+    
+    
+</body>
+</html>
